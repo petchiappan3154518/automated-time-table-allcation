5547d90fbce48daacee01b58d1fcf0be9f19f1a9
petchiappancoder <59636243+petchiappancoder@users.noreply.github.com>
2020-01-08 11:11:21 +0530

    sample user input data

---
+++ sample data.ts
@@ -0,0 +1,24 @@
+let teachersData = [
+  { id: "001", subjects: ["tamil", "english"], name: "sfyds" },
+  { id: "002", subjects: ["english", "math"] },
+  { id: "003", subjects: ["scie", "soci"] },
+  { id: "004", subjects: ["tamil", "soci"] },
+  { id: "005", subjects: ["english", "soci"] },
+  { id: "006", subjects: ["tamil", "soci"] },
+  { id: "007", subjects: ["english", "socil"] },
+  { id: "008", subjects: ["english", "soci"] }
+];
+
+let a = [
+  { name: "I" },
+  { name: "II" },
+  { name: "III" },
+  { name: "IV" },
+  { name: "V" },
+  { name: "VI" },
+  { name: "VII" },
+  { name: "VIII" }
+];
+
+let y = [];
+let z = [];
