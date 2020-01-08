8069a01e7d997244234090a5263e88cca3cdb376
petchiappancoder <59636243+petchiappancoder@users.noreply.github.com>
2020-01-08 11:09:53 +0530

    automated timetable

---
+++ main.ts
@@ -0,0 +1,154 @@
+// Teacher Class
+
+class Teacher {
+  constructor(
+    public id: string,
+    public subjects: Array<string>,
+    public day: number,
+    public hour: number
+  ) {}
+}
+
+// ClassRoom Class
+
+class Classroom {
+  teacher: Teacher;
+  constructor(public name: string, public day: number, public hour: number) {}
+}
+
+// School Class
+
+class School {
+  classRooms: Array<Classroom> = [];
+
+  teachers: Array<Teacher> = [];
+
+  constructor(
+    public name: string,
+    public workingDays: number,
+    public hours: number
+  ) {}
+
+// Import Teachers
+
+  importTeachers(arr: Array<{ id: string; subjects: Array<string> }>) {
+    for (const x of arr) {
+      for (let i = 0; i < this.workingDays; i++) {
+        for (let j = 0; j < this.hours; j++) {
+          const teacher = new Teacher(x.id, x.subjects, i + 1, j + 1);
+          this.teachers.push(teacher);
+        }
+      }
+    }
+  }
+
+// Add ClassRooms
+
+  addClassRoom(name: string) {
+    for (let i = 0; i < this.workingDays; i++) {
+      for (let j = 0; j < this.hours; j++) {
+        const classRoom = new Classroom(name, i + 1, j + 1);
+        this.classRooms.push(classRoom);
+      }
+    }
+  }
+
+// Teachers Shuffle
+
+  Shuffle() {
+    this.teachers = this.teachers.sort(() => Math.random() - 0.5);
+  }
+
+// Assign Teacher to Class
+
+  assign(x) {
+    const teacfound = this.teachers.filter(
+      teacher => teacher.day == x.day && teacher.hour == x.hour
+    );
+
+    for (let j = 0; j < 1; j++) {
+      x.teacher = teacfound[j];
+    }
+  }
+
+  start() {
+    this.Shuffle();
+
+    for (let x of this.classRooms) {
+      let s = this.assign(x);
+    }
+  }
+}
+
+const sample = new School("Sample", 6, 8);
+
+sample.importTeachers(teachersData);
+sample.addClassRoom("I");
+sample.addClassRoom("II");
+sample.addClassRoom("III");
+sample.addClassRoom("IV");
+sample.addClassRoom("V");
+sample.addClassRoom("VI");
+sample.addClassRoom("VII");
+sample.addClassRoom("VIII");
+
+sample.start();
+
+//divide classRoom day
+
+function daydiv(t) {
+  for (let i = 0; i < 1; i++) {
+    var rowfound = t.filter(teachers => teachers.day == i + 1);
+    z.push(rowfound);
+  }
+  y.push(z);
+} 
+
+// divide classRooms
+
+function clsdiv(data) {
+  for (var i = 0; i < 6; i++) {
+    var clsfound = data.filter(teachers1 => teachers1.name == a[i].name);
+    daydiv(clsfound);
+  }
+}
+
+clsdiv(sample.classRooms);
+
+//create cell <td>
+
+const createColumn = (data) => {
+  const td = document.createElement('td')
+  const text = document.createTextNode(data.teacher.hour);
+  td.appendChild(text)
+  return td;
+}
+
+//create row <tr>
+
+const createRow = (data) => {
+  const tr = document.createElement('tr');
+  for (const i of data) {
+    const col = createColumn(i);
+    tr.appendChild(col)
+  }
+  return tr;
+}
+
+// create table <table>
+
+function createTable(data){
+  const table = document.createElement('table');
+  table.setAttribute("border",'2');
+  for (const i of data) {
+    const col = createRow(i);
+    table.appendChild(col)
+  }
+  return table;
+}
+
+for(let l of y){
+  console.log(l);
+  const table2 = createTable(l);
+  document.getElementById('app').appendChild(table2);
+}
