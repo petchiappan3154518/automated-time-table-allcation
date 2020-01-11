bd54d7ef42ec2506b38fe984e35edc919bb85d70
Raja Gowtham <42373586+RajaGowtham@users.noreply.github.com>
2020-01-11 15:42:52 +0530

    updated

--- main.ts
+++ main.ts
@@ -1,29 +1,3 @@
-import "./style.css";
-let teachersData = [
-  { id: "001", subjects: ["tamil", "english"], name: "sfyds" },
-  { id: "002", subjects: ["english", "math"] },
-  { id: "003", subjects: ["scie", "soci"] },
-  { id: "004", subjects: ["tamil", "soci"] },
-  { id: "005", subjects: ["english", "soci"] },
-  { id: "006", subjects: ["tamil", "soci"] },
-  { id: "007", subjects: ["english", "socil"] },
-  { id: "008", subjects: ["english", "soci"] }
-];
-
-let a = [
-  { name: "I" },
-  { name: "II" },
-  { name: "III" },
-  { name: "IV" },
-  { name: "V" },
-  { name: "VI" },
-  { name: "VII" },
-  { name: "VIII" }
-];
-
-let y = [];
-let z = [];
-
 // Teacher Class
 
 class Teacher {
@@ -52,7 +26,7 @@ class School {
   constructor(
     public name: string,
     public workingDays: number,
-    public hours: number
+    public hours: number//323213257uhgfhtfh
   ) {}
 
 // Import Teachers
